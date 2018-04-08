a9f0ad0b02d52c59c8ca33d1c19d570b7379374b
Waweru <developer.waweru@gmail.com>
2018-04-08 11:02:39 +0300

    Creating the back-end section

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+// Back End Section
+var num = 0,
+    newMark = 0,
+    playerDetails = [],
+    finalScore = 0,
+    diePic = "",
+    pos = 0;
+
+function PlayersInfo(name, score, totalScore) {
+    this.playerNames = name;
+    this.playerMarks = score;
+    this.totalScores = totalScore;
+}
+var genRandom = function () {
+    randomNo = Math.floor(Math.random() * 6) + 1;
+    return randomNo;
+}
+PlayersInfo.prototype.AddScores = function (thisMark) {
+    if (thisMark === 1) {
+        this.playerMarks = 0;
+    } else if (thisMark !== 1) {
+        this.playerMarks = this.playerMarks + thisMark;
+    }
+    return this.playerMarks;
+}
+PlayersInfo.prototype.Total = function (total) {
+    return this.totalScores = this.totalScores + total;
+}
+var getDieSide = function (getInput) {
+    if (getInput == 1) diePic = "img/dice/1.jpg";
+    else if (getInput == 2) diePic = "img/dice/2.jpg";
+    else if (getInput == 3) diePic = "img/dice/3.jpg";
+    else if (getInput == 4) diePic = "img/dice/4.jpg";
+    else if (getInput == 5) diePic = "img/dice/5.jpg";
+    else if (getInput == 6) diePic = "img/dice/6.jpg";
+
+    return diePic;
+}
+
+function reset() {
+    pos = 0;
+    PlayersInfo.playerMarks = 0;
+    PlayersInfo.totalScores = 0;
+    $("#image-die").html("");
+    $("p.text-uppercase").text("");
+    $("h1").text("0");
+    $(".cumulative").text("");
+}
